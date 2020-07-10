1add2ab74a74b3ca9fad659afa0294e8e8f249c8
Anderson Speed <andersonspeed105@gmail.com>
2020-07-10 13:02:31 -0400

    Added Basic User authentication

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
 class HomePageController < ApplicationController
   def index
+    if current_user.nil?
+      render 'signin'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home_page/index'
   resources :topics
   root 'home_page#index'
