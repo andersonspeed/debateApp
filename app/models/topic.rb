d5c85ff65a70e058b280a214784e83bb1c9f14be
Anderson Speed <andersonspeed105@gmail.com>
2020-07-30 16:14:26 -0400

    showed ben some stuff

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :owner, class_name: :User
   belongs_to :category
   has_many :comments
+  belongs_to :challenger
   
   validates :numReplies, presence: true, numericality: { only_integer: true }
   validates :wager, presence: true, numericality: true
